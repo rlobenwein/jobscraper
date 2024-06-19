f974cda486625ac33dc512fa5304a913b702a1e8
Rodrigo Lobenwein <rodrigolobenweindev@gmail.com>
2024-06-19 20:21:57 -0300

    feat: adicionar definições de tipos

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+type ScraperParams = {
+    url: string;
+    containerSelector: string;
+    titleSelector: string;
+    descriptionSelector: string;
+    linkSelector: string;
+    searchTerms: string[];
+    fullContainerSelector:string,
+    companyName:string,
+    location:string
+  }
+
+
+  
